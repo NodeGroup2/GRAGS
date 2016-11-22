67747def7228e85914229893d382f04a382737fa
msachi <marchee88@gmail.com>
2016-11-22 18:27:51 +0000

    Link input field search to making a back-end request, and improve styling

--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
+console.log('got into index')
+
+var inputField = document.querySelector(".input_field");
+var searchBtn = document.querySelector(".search_btn");
 var recipes = document.getElementsByClassName("recipes");
 
 for(i = 0; i < recipes.length; i++){
@@ -5,4 +9,25 @@ for(i = 0; i < recipes.length; i++){
     document.getElementsByClassName("first_page")[0].style.visibility = "hidden";
     document.getElementsByClassName("second_page")[0].style.visibility = "visible";
   });
+}
+
+searchBtn.onclick = function() {
+  console.log('clicked')
+  if (inputField.value) {
+    sendRequest();
+  }
+}
+
+function sendRequest() {
+  var url = '/recipes/?q=' + inputField.value;
+  console.log(url);
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      document.body.innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('GET', url, true);
+  // xhr.responseType = "document";
+  xhr.send();
 }
