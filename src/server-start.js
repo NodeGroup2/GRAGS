d586e4629c339107f8f294884ee96a473fcc935b
Shireen Zoaby <shireen.zoaby@gmail.com>
2016-11-22 19:24:39 +0000

    try adding api key to headers

--- src/server-start.js
+++ src/server-start.js
@@ -1,6 +1,7 @@
 const server = require('./server.js');
 
 server.start(err => {
+  console.log(process.env.TESCO_API_KEY);
   if (err) throw err;
   console.log(`server is running on ${server.info.uri}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const Hapi = require('hapi');
 const vision = require('vision');
 const inert = require('inert');
@@ -5,6 +7,7 @@ const inert = require('inert');
 const server = new Hapi.Server();
 const Request = require('request');
 const RecipesList = [];
+const env = require('env2')('./.env');
 
 server.connection({
   host: 'localhost',
@@ -67,13 +70,29 @@ const routes = [
     method: 'GET',
     path: '/recipe/', //TODO change recipe to the actual name of the recipe chosen
     handler: function(request, reply) {
-      let recipe = RecipesList[request.query.n];
-      let url = 'https://dev.tescolabs.com/grocery/products/';
-      console.log(recipe);
+      let recipe = {ingredients: ["chicken", "rice", "tomato"]};
+      let url = 'https://dev.tescolabs.com/grocery/products/?query=chicken&offset=0&limit=1';
+      let options = {
+         url: 'https://dev.tescolabs.com/grocery/products/',
+         headers: {
+          'Ocp-Apim-Subscription-Key': process.env.TESCO_API_KEY
+        }
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var info = JSON.parse(body);
+          console.log(info);
+        }
+      }
+
+      Request(options, callback);
       return;
+      for(let i=0;i<recipe.ingredients.length;i++){
+        Request(
+        )
+      }
 
-
-      for(let i=0;i<recipe.ingredients.length;i++){} //close me
       Request(`${url}?query=${recipe.ingredients[i]}&offset=0&limit=1`, function(err, res, body) {
         var json = JSON.parse(body);
         for (var i=0; i<3; i++) {
