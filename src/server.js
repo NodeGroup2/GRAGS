934668ade2569b1712810ce67eb3bd6fb864b171
marisid <sid_marina@hotmail.com>
2016-11-22 15:59:30 +0000

    Create RecipesList from Recipe Puppy API data

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 const Request = require('request');
+const RecipesList = [];
 
 server.connection({
   host: 'localhost',
@@ -29,6 +30,17 @@ server.route({
     let searchRecipe = request.query.q
     Request(`${url}?q=${searchRecipe}`, function(err, res, body) {
       console.log(body);
+      // run function to store top three recipes and their ingredients
+      var json = JSON.parse(body);
+
+      for (var i=0; i<3; i++) {
+        RecipesList[i] = {
+          "title": json.results[i].title,
+          "ingredients": json.results[i].ingredients
+        }
+      }
+      console.log(RecipesList);
+      // reply with a rendered template
       reply(body);
     })
   }
