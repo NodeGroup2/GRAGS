3e7113dd0ea769e5d938bf50731ebe33fe3b4683
Shireen Zoaby <shireen.zoaby@gmail.com>
2016-11-23 10:48:45 +0000

    Remove console logging the Tesco API key

--- src/server-start.js
+++ src/server-start.js
@@ -1,7 +1,6 @@
 const server = require('./server.js');
 
 server.start(err => {
-  console.log(process.env.TESCO_API_KEY);
   if (err) throw err;
   console.log(`server is running on ${server.info.uri}`);
 });
