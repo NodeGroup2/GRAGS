406f08490cc217810a90b9248fdc2fd061bdce2b
Shireen Zoaby <shireen.zoaby@gmail.com>
2016-11-23 10:42:18 +0000

    make requests to second api with ingredients

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const inert = require('inert');
 const server = new Hapi.Server();
 const Request = require('request');
 const RecipesList = [];
+var ingredients = [];
 const env = require('env2')('./.env');
 
 server.connection({
@@ -71,42 +72,52 @@ const routes = [
     path: '/recipe/', //TODO change recipe to the actual name of the recipe chosen
     handler: function(request, reply) {
       let recipe = {ingredients: ["chicken", "rice", "tomato"]};
-      let url = 'https://dev.tescolabs.com/grocery/products/?query=chicken&offset=0&limit=1';
+      // let url = 'https://dev.tescolabs.com/grocery/products/?query=chicken&offset=0&limit=1';
       let options = {
-         url: 'https://dev.tescolabs.com/grocery/products/',
+         url: 'https://dev.tescolabs.com/grocery/products/?query=nada&offset=0&limit=1',
          headers: {
           'Ocp-Apim-Subscription-Key': process.env.TESCO_API_KEY
         }
       };
 
       function callback(error, response, body) {
+        //console.log("I'm in the callback and I'm getting this res:", response);
         if (!error && response.statusCode == 200) {
           var info = JSON.parse(body);
-          console.log(info);
+          addIngredientToArray(info);
+          console.log(ingredients);
         }
       }
 
-      Request(options, callback);
-      return;
       for(let i=0;i<recipe.ingredients.length;i++){
-        Request(
-        )
+        options.url = "https://dev.tescolabs.com/grocery/products/?query="+recipe.ingredients[i]+"&offset=0&limit=1";
+        Request(options, callback);
       }
 
-      Request(`${url}?query=${recipe.ingredients[i]}&offset=0&limit=1`, function(err, res, body) {
-        var json = JSON.parse(body);
-        for (var i=0; i<3; i++) {
-          RecipesList[i] = {
-            "title": json.results[i].title,
-            "ingredients": json.results[i].ingredients.split(','),
-            "link": json.results[i].href
-          }
-        }
-        console.log(RecipesList);
-        reply().redirect('/');
-      })
+      function addIngredientToArray(response){
+        var body = response.uk.ghs.products.results[0];
+        var info = {
+          image: body.image,
+          name: body.name,
+          price: body.price
+        } // TODO add unit price
+        ingredients.push(info);
+      }
     }
-   }
+
+      // Request(`${url}?query=${recipe.ingredients[i]}&offset=0&limit=1`, function(err, res, body) {
+      //   var json = JSON.parse(body);
+      //   for (var i=0; i<3; i++) {
+      //     RecipesList[i] = {
+      //       "title": json.results[i].title,
+      //       "ingredients": json.results[i].ingredients.split(','),
+      //       "link": json.results[i].href
+      //     }
+      //   }
+      //   console.log(RecipesList);
+      //   reply().redirect('/');
+      // })
+    }
 ];
 
 server.route(routes);
